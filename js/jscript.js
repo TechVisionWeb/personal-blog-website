70b4e516397e60803c4bc743a132d29e2ff664d1
Charlotte Norford <techvisionweb@yahoo.com>
2024-11-07 11:27:13 +0000

    Edit js to enable the menu open and close,style the menu & make it responsive

--- js/jscript.js
+++ js/jscript.js
@@ -3,10 +3,12 @@ function toggleMenuDisplay() {
     mobileMedia = window.matchMedia("(min-width: 1101px)")
     var menuOpenButton = document.querySelector(".nav_menu-btn--open");
     var menuCloseButton = document.querySelector(".nav_menu-btn--close");
-
+    var menu = document.querySelector(".nav-menu");
     if (mobileMedia.matches) { // If media query matches
         changeDisplayCSS(menuOpenButton, "nav_menu-btn--show", "nav_menu-btn--hide");
         changeDisplayCSS(menuCloseButton, "nav_menu-btn--show", "nav_menu-btn--hide" );
+        changeDisplayCSS(menu, "nav_menu--hide", "nav_menu--show");
+
     }
     else {
         // Only change the display of a menu button to the default burger bar when both are hidden.
@@ -15,8 +17,10 @@ function toggleMenuDisplay() {
             && menuCloseButton.classList.contains("nav_menu-btn--hide")) {
 
                 changeDisplayCSS(menuOpenButton,"nav_menu-btn--hide", "nav_menu-btn--show" );
+                changeDisplayCSS(menu, "nav_menu--show", "nav_menu--hide");
+
         }
-      
+
     }
 }
 
@@ -29,11 +33,13 @@ function toggleMenuSymbol() {
 }
 
 function showOrHideElement(menuOpenButton, menuCloseButton) {
+    var menu = document.getElementsByClassName("nav-menu")[0];
 
     if (menuOpenButton.classList.contains("nav_menu-btn--show")) {
 
         changeDisplayCSS(menuOpenButton, "nav_menu-btn--show", "nav_menu-btn--hide");
         changeDisplayCSS(menuCloseButton, "nav_menu-btn--hide", "nav_menu-btn--show" );
+        changeDisplayCSS(menu, "nav_menu--hide", "nav_menu--show");
 
     }
 
@@ -41,6 +47,7 @@ function showOrHideElement(menuOpenButton, menuCloseButton) {
 
         changeDisplayCSS(menuCloseButton, "nav_menu-btn--show", "nav_menu-btn--hide" );
         changeDisplayCSS(menuOpenButton, "nav_menu-btn--hide", "nav_menu-btn--show" );
+        changeDisplayCSS(menu, "nav_menu--show", "nav_menu--hide");
     }
 }
 
